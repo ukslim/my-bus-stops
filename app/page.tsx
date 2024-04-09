725e75977fc0aaaf3af4e2da7230a593fac231a9
John Hartnup <john.hartnup@wealthwizards.com>
2024-04-09 22:05:18 +0100

    Vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,7 @@ export default function Home() {
   return (
     <main className="min-h-screen items-center p-0 sm:p-25 sm:text-base">
       <h1 className="text-3xl font-bold mt-1 sm:mt-4">Buses</h1>
-      {
-        config.length === 0 ? <div>No bus stops configured</div> : null
-      }
+      {config.length === 0 ? <div>No bus stops configured</div> : null}
       <div className="w-full text-sm sm:text-base">
         {config.map((id) => (
           <BusStop key={id} busStopId={id} refreshTrigger={refreshTrigger} />
