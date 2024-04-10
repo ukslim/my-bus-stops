cf909305638186e66b98b90c080b331a6051a6d0
John Hartnup <john.hartnup@wealthwizards.com>
2024-04-10 10:27:26 +0100

    Comments and link back to github

--- pages/api/times.ts
+++ pages/api/times.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// Purely a proxy for CORS reasons
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
--- pages/config.tsx
+++ pages/config.tsx
@@ -24,6 +24,7 @@ export default function Home() {
             <h1>Acknowledgements</h1>
             <p>We use the API from <a href="https://bustimes.org">bustimes.org</a> which themselves
             make use of public APIs they credit on their own page. Thank you everyone.</p>
+            <p>Source is in <a href="https://github.com/ukslim/my-bus-stops">GitHub</a>.</p>
             <h1>Configure Bus Stops</h1>
             <p>You can find bus stop IDs on <a href="https://bustimes.org">bustimes.org</a></p>
             <p>Enter them here, comma separated and click to save.</p>
