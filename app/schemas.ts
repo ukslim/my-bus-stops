fd3848fb5854e9052b5643f104d5a2f896433473
John Hartnup <john.hartnup@wealthwizards.com>
2024-04-10 14:09:18 +0100

    Comment unused props in schemas

--- app/schemas.ts
+++ app/schemas.ts
@@ -2,29 +2,30 @@ import { z } from "zod";
 
 export const configSchema = z.array(z.string());
 export const timesSchema = z.object({
+  // Unused fields are commented
   times: z.array(
     z.object({
       id: z.number(),
-      trip_id: z.number(),
+      //   trip_id: z.number(),
       service: z.object({
         line_name: z.string(),
-        operators: z.array(
-          z.object({
-            id: z.string(),
-            name: z.string(),
-            parent: z.string(),
-            vehicle_mode: z.string(),
-          }),
-        ),
+        // operators: z.array(
+        //   z.object({
+        //     id: z.string(),
+        //     name: z.string(),
+        //     parent: z.string(),
+        //     vehicle_mode: z.string(),
+        //   }),
+        // ),
       }),
       destination: z.object({
-        atco_code: z.string(),
+        // atco_code: z.string(),
         name: z.string(),
-        locality: z.string(),
+        // locality: z.string(),
       }),
       aimed_arrival_time: z.coerce.date().optional(),
       aimed_departure_time: z.coerce.date().optional(),
-      delay: z.string().optional(),
+      //   delay: z.string().optional(),
       expected_departure_time: z.coerce.date().optional(),
       expected_arrival_time: z.coerce.date().optional(),
     }),
@@ -32,11 +33,11 @@ export const timesSchema = z.object({
 });
 
 export const stopSchema = z.object({
-  atco_code: z.string(),
-  naptan_code: z.string(),
+  // Unused fields are commented
+  //   atco_code: z.string(),
+  //   naptan_code: z.string(),
   common_name: z.string(),
   long_name: z.string(),
-  // Not using these fields...
   // "location": z.array(z.number()),
   // "indicator": "Opp",
   // "icon": null,
